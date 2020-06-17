30112618588aabbd0bbfef7c16e6b3e7c66eb8c8
AKyouma <augustopta32@gmail.com>
2020-06-17 01:02:27 -0300

    Primeira tentativa, 2/5

--- src/main.c
+++ src/main.c
@@ -1,17 +1,80 @@
-/* Contador de palavras
- *
- * Este programa recebera uma serie de caracteres representando palavras em sua
- * entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
- * numero de palavras separadas que recebeu e, apos, encerrar.
+/* Contador de primos com multi-threads
+ * Data de  submissao: 17/06/20
+ * Nome: Augusto Lourenço de Faria
+ * RA: 194447
  */
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <pthread.h>
+
+#define n_threads 4
+
+pthread_mutex_t trava;
+int resultado=0;
+
+// função retorna 0 se não for primo e 1 se for primo
+int primo (int numero){
+	int i;
+	if (numero == 1 || numero == 0)
+		return 0;
+	for (i=2; i < numero; i++){
+		if (numero % i == 0)
+			return 0;
+	}
+	return 1;
+}
+
+//cria a função thread para travar e as demais threads não aumentarem o resultado sem a anterior terminar
+void *thread(void *args){
+
+	int num = *((int *)args);
+	int num_avaliado = primo(num);
+	pthread_mutex_lock(&trava);
+	resultado += num_avaliado;
+	pthread_mutex_unlock(&trava);
+	return NULL;
+}
 
 int main() {
 
-  int x, y;
+	pthread_t t[n_threads] = {0, 0, 0, 0};
+	int numero, filhos, vazia=0, i=0;
+	
 
-  scanf("%d %d\n", &x, &y);
-  printf("%d\n", x + 200);
+	do{
+		scanf("%d", &numero);
+
+		//verifica se já tem 4 threads filhos, se sim espera um acabar para continuar
+		if (filhos == 4){
+			while (i < 4){		
+				//espera acabar
+				pthread_join(t[i], NULL);
+				//seta novamente pra 0, ou seja está disponível
+				t[i]=0;
+				filhos--;
+				i++;
+			}	
+		}
+		
+		for(i=0; i < 4; i++){
+			//verifica qual dos threads está vazio, ou seja setado com 0
+			if(t[i] == 0){
+				vazia=i;
+				i=4;
+			}
+		}
+
+		//após a verificação de qual está vazio, cria o thread no endereço t[vazio]
+		pthread_create(&(t[vazia]), NULL, thread, ((void *) (&numero)));
+		filhos++;
+		
+	}while(getchar() != '\n');
+	
+	//espera todos os threads acabarem	
+	for(int i = 0; i < 4; i++)
+		pthread_join(t[i], NULL);
+		
+	printf("%d\n", resultado);
   return 0;
 }
